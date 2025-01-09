0e7bdad6b170859f8a955fcfaea17ea48eb09cb1
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-09 08:00:16 +0100

    feat[pari-e-dispari]

---
+++ palidroma/script.js
@@ -0,0 +1,28 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedere all’utente di inserire una parola
+let parola = prompt("inserisci parola:");
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function èPalindroma(parola) {
+
+    // trasformo parola in miniscolo e rimuovo spazi
+    let parolaPulita = parola.toLowerCase().replace(/\s+/g, '');
+
+    // inverto parola
+    let parolaInvertita = parolaPulita.split('').reverse().join('');
+
+    // risultato true se la parola è unguale all'inversa, altrimenti false
+    return parolaPulita === parolaInvertita;
+}
+
+//  funzione input utente risultato
+if (èPalindroma(parola)) {
+    console.log("la parola è palindroma.");
+    
+} else {
+    console.log("la parola non è palindroma.");
+    
+}
